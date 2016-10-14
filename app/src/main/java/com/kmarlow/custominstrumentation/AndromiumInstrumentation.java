2433ee0c45dc90ce9de1118ba29a2b749d349572
Kevin Marlow <kevinmmarlow@gmail.com>
2016-10-14 18:08:17 +0800

    Intercept activity creation, check stack

--- app/src/main/java/com/kmarlow/custominstrumentation/AndromiumInstrumentation.java
+++ app/src/main/java/com/kmarlow/custominstrumentation/AndromiumInstrumentation.java
@@ -6,38 +6,78 @@ import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.os.UserHandle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 
 public class AndromiumInstrumentation extends Instrumentation {
     private static final String TAG = AndromiumInstrumentation.class.getSimpleName();
 
     public AndromiumInstrumentation() {
-
     }
 
     // Intercept Activity construction
 
-//    public ActivityResult execStartActivity(
-//            Context who, IBinder contextThread, IBinder token, Activity target,
-//            Intent intent, int requestCode, Bundle options) {
-//        return null;
-//    }
+    public ActivityResult execStartActivity(
+            Context who, IBinder contextThread, IBinder token, Activity target,
+            Intent intent, int requestCode, Bundle options) {
+        Toast.makeText(who, "Start " + intent.getComponent().getShortClassName(), Toast.LENGTH_SHORT).show();
+
+//        try {
+//            Class<Instrumentation> instrumentation = (Class<Instrumentation>) getClass().getSuperclass();
+//            Method execStartActivity = instrumentation.getDeclaredMethod("execStartActivity", new Class[]{Context.class, IBinder.class, IBinder.class, Activity.class, Intent.class, int.class, Bundle.class});
+//            execStartActivity.setAccessible(true);
 //
-//    public ActivityResult execStartActivity(
-//            Context who, IBinder contextThread, IBinder token, String target,
-//            Intent intent, int requestCode, Bundle options) {
-//        return null;
-//    }
+//            return (ActivityResult) execStartActivity.invoke(this, who, contextThread, token, target, intent, requestCode, options);
 //
-//    public ActivityResult execStartActivity(
-//            Context who, IBinder contextThread, IBinder token, Activity target,
-//            Intent intent, int requestCode, Bundle options, UserHandle user) {
-//        return null;
-//    }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        return null;
+    }
+
+    public ActivityResult execStartActivity(
+            Context who, IBinder contextThread, IBinder token, String target,
+            Intent intent, int requestCode, Bundle options) {
+        Toast.makeText(who, "Start " + intent.getComponent().getShortClassName(), Toast.LENGTH_SHORT).show();
+
+//        try {
+//            Class<Instrumentation> instrumentation = (Class<Instrumentation>) getClass().getSuperclass();
+//            Method execStartActivity = instrumentation.getDeclaredMethod("execStartActivity", new Class[]{Context.class, IBinder.class, IBinder.class, String.class, Intent.class, int.class, Bundle.class});
+//            execStartActivity.setAccessible(true);
+//
+//            return (ActivityResult) execStartActivity.invoke(this, who, contextThread, token, target, intent, requestCode, options);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        return null;
+    }
+
+    public ActivityResult execStartActivity(
+            Context who, IBinder contextThread, IBinder token, Activity target,
+            Intent intent, int requestCode, Bundle options, UserHandle user) {
+        Toast.makeText(who, "Start " + intent.getComponent().getShortClassName(), Toast.LENGTH_SHORT).show();
+
+//        try {
+//            Class<Instrumentation> instrumentation = (Class<Instrumentation>) getClass().getSuperclass();
+//            Method execStartActivity = instrumentation.getDeclaredMethod("execStartActivity", new Class[]{Context.class, IBinder.class, IBinder.class, Activity.class, Intent.class, int.class, Bundle.class, UserHandle.class});
+//            execStartActivity.setAccessible(true);
+//
+//            return (ActivityResult) execStartActivity.invoke(this, who, contextThread, token, target, intent, requestCode, options, user);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        return null;
+    }
 
 
     // Intercept activity attach
--- app/src/main/java/com/kmarlow/custominstrumentation/SubActivity.java
+++ app/src/main/java/com/kmarlow/custominstrumentation/SubActivity.java
@@ -1,13 +1,26 @@
 package com.kmarlow.custominstrumentation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+/**
+ * Run this to print out the stack.
+ * adb shell dumpsys activity activities | sed -En -e '/Stack #/p' -e '/Running activities/,/Run #0/p'
+ */
 public class SubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SubActivity.this, ThirdActivity.class));
+            }
+        });
     }
 }
