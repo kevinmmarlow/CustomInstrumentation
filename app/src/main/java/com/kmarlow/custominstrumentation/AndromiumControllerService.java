de9e5315e3e48945baa59cbeb267115fe475ab6d
Jesse Sum <jessehysum@gmail.com>
2016-10-18 22:59:58 +0800

    testing the service

--- app/src/main/java/com/kmarlow/custominstrumentation/AndromiumControllerService.java
+++ app/src/main/java/com/kmarlow/custominstrumentation/AndromiumControllerService.java
@@ -1,24 +1,55 @@
 package com.kmarlow.custominstrumentation;
 
-import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.view.View;
 
-public class AndromiumControllerService extends Service {
+import com.andromium.framework.AndromiumApi;
+import com.andromium.framework.ui.AndromiumHackFrameworkStub;
+import com.andromium.framework.ui.WindowConfig;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        AndromiumInstrumentation andromiumInstrumentation = AndromiumInstrumentationInjector.inject(this);
+public class AndromiumControllerService extends AndromiumHackFrameworkStub {
 
-        Intent intent = new Intent(this, SubActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
+    private AndromiumControllerServiceImpl app;
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public AndromiumApi getAndromiumAppInstance(int appId) {
+        if (app == null) {
+            app = new AndromiumControllerServiceImpl(this, launchIntent, appId);
+        }
+        return app;
+    }
+}
+
+class AndromiumControllerServiceImpl extends AndromiumApi {
+
+    AndromiumControllerServiceImpl(AndromiumControllerService context, Intent launchIntent, int appId) {
+        super(context, launchIntent, appId);
+        AndromiumInstrumentation andromiumInstrumentation = AndromiumInstrumentationInjector.inject(context);
+
+        Intent intent = new Intent(context, SubActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
+    @Override
+    public int getAppBodyLayoutXml() {
+        return 0;
+    }
+
+    @Override
+    public void initializeAndPopulateBody(View view) {
+
+    }
+
+    @Override
+    public WindowConfig getWindowConfiguration() {
+        return new WindowConfig(600, 500, true);
     }
 }
